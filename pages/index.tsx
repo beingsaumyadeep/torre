30c83185c1c05dccc196f25e0a776272a1a3b7e7
Saumyadeep Sarkar <saumyadeeps1@gmail.com>
2020-12-27 19:45:06 +0530

    Init app

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styled from 'styled-components'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Torre - Professional Job Network</title>
+      </Head>
+      <h1>Welcome to Torre</h1>
+    </>
+  )
+}
